29c28e5f4639cefd417120febdaf51c3afd9c277
Aniket Nath <101539387+aniket2001github@users.noreply.github.com>
2023-11-15 19:00:07 +0530

    dg

--- mail.php
+++ mail.php
@@ -1,51 +1,50 @@
 <?php
 
-    // Only process POST requests.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-		$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);  
-        $message = trim($_POST["message"]);
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Please complete the form and try again.";
-            exit;
-        }
+if(isset($_POST['Send']))
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "kaiumhossainrian@gmail.com";
+//Load Composer's autoloader
+require 'PHP Mailer/Exception.php';  
+require 'PHP Mailer/PHPMailer.php';  
+require 'PHP Mailer/SMTP.php';  
 
-        //Set the email subject.
-        $subject = "New contact from $name";
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-        // Build the email content.
-        $email_content = "Name: $name\n";  
-        $email_content .= "Email: $email\n\n"; 
-        $email_content .= "Message:\n$message\n";
+try {
 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
+    $name = $_POST['Name'];
+    $email = $_POST['E-mail'];
+    $msg = $_POST['message'];
+    
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'nathaniket678@gmail.com';                     //SMTP username
+    $mail->Password   = 'uoblpafirnoqlbpg';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-        // Send the email.
-        if (mail($recipient,$subject, $email_content)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
+    //Recipients
+    $mail->setFrom('nathaniket678@gmail.com', 'Contact Form');
+    $mail->addAddress('nathaniket678@gmail.com', 'Joe User');     //Add a recipient
+   
 
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
-    }
 
-?>
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = "Sender Name: $name <br> Sender Email: $email <br> Sender Massege: $msg";
+    
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
